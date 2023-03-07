c08d8c51d6eedfad85d5a163b4a16831ae892a66
bun <imaizumi.taiki@classmethod.jp>
2023-03-07 13:28:23 +0900

    feat: ルールリントとルール作成用のファイル分け

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,15 @@
 const readFileSync = require("fs").readFileSync;
-import { join } from "path";
+const prh = require("textlint-rule-prh");
 import type { TextlintRuleModule } from "@textlint/types";
-import * as prh from "textlint-rule-prh";
-import { AwsServices } from "./regularRule/awsServices";
-import { FetchResponse } from "./regularRule/fetchResponse";
-import { Rules } from "./regularRule/ruleFile";
 
 export interface Options {
   allows?: string[];
 }
 
 export const report: TextlintRuleModule<Options> = (context) => {
-  const ruleFilePath = join("dict", "aws_service.yml");
+  const ruleFilePath = "prh.yml";
   const ruleContent = readFileSync(ruleFilePath, "utf-8");
   return prh.fixer(context, {
     ruleContents: [ruleContent],
   });
 };
-
-const hey = async () => {
-  const fetchResponse = new FetchResponse();
-  const awsServices = new AwsServices(fetchResponse);
-  const services = await awsServices.get();
-  const rules = new Rules(services);
-  console.log(JSON.stringify(rules.get()));
-};
-hey();
