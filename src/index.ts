78cf8e96836b4b36d7e7db0b6d8c841c51741586
bun <imaizumi.taiki@classmethod.jp>
2023-03-08 01:22:52 +0900

    feat: npmへの公開用の設定を追記

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,20 @@ export interface Options {
   allows?: string[];
 }
 
-export const report: TextlintRuleModule<Options> = (context) => {
-  const ruleFilePath = join(__dirname, "..", "dict", "auto-create-regulaer-rules.yml");
+const report: TextlintRuleModule<Options> = (context) => {
+  const ruleFilePath = join(
+    __dirname,
+    "..",
+    "dict",
+    "auto-create-regulaer-rules.yml"
+  );
   const ruleContent = readFileSync(ruleFilePath, "utf-8");
   return prh.fixer(context, {
     ruleContents: [ruleContent],
   });
 };
+
+export default {
+  linter: report,
+  fixer: report
+}
