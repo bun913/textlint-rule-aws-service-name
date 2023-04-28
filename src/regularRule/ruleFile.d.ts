6c9cf04666e7f34cc1a2acd7819ffe98f36a6865
bun913 <imaizumi.taiki@classmethod.jp>
2023-04-28 11:28:53 +0900

    chore: add gitignore d.ts files

--- src/regularRule/ruleFile.d.ts
+++
@@ -1,7 +0,0 @@
-import { Rules } from "./rules";
-export declare class RuleFile {
-    readonly rules: Rules;
-    readonly RegularFilePath: string;
-    constructor(rules: Rules);
-    createRegularRuleFile(): string;
-}
